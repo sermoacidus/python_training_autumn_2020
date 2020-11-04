fd6bb97e5b240b68c9e0306a21b4e7e1cbde2660
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2020-11-04 22:37:34 +0300

    homework (29/10 lection)

--- homework1/tests/test_task3_max_and_min.py
+++ homework1/tests/test_task3_max_and_min.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from task3.max_and_min import find_maximum_and_minimum
 
@@ -5,9 +7,9 @@ from task3.max_and_min import find_maximum_and_minimum
 @pytest.mark.parametrize(
     ["value", "expected_result"],
     [
-        ("tests\\testfile_for_task3_1.txt", (0, 551515)),
-        ("tests\\testfile_for_task3_2.txt", (-6465166, 7984098)),
-        ("tests\\testfile_for_task3_3.txt", (1, 1)),
+        (Path("tests/testfile_for_task3_1.txt"), (0, 551515)),
+        (Path("tests/testfile_for_task3_2.txt"), (-6465166, 7984098)),
+        (Path("tests/testfile_for_task3_3.txt"), (1, 1)),
     ],
 )
 def test_max_and_min(value: str, expected_result: tuple):
