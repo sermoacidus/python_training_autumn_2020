4fdaf821fd4a2bc48528c758cc33912a9ddcdf09
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2020-11-18 19:34:51 +0300

    added info about bug fixes

--- homework3/tasks_hw3/task3.py
+++ homework3/tasks_hw3/task3.py
@@ -1,3 +1,16 @@
+"""
+Changes made:
+    in class Filter:
+        - "__init__(functions)" to "__init__(*functions)", so it can take several separate functions
+
+    in method make_filter:
+        - "keyword_filter_func(value)" to "keyword_filter_func(key, value)"
+        - "return value[key] == value" to "return lambda data_set: value == data_set[key]"
+        - "filter_funcs.append(keyword_filter_func)" to "filter_funcs.append(keyword_filter_func(key, value))"
+        - "return Filter(filter_funcs)" to "return Filter(*filter_funcs)"
+"""
+
+
 class Filter:
     """
     Helper filter class. Accepts a list of single-argument
