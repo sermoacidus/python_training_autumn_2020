618fdbe0d9ab97832e6aa2d6cb473f34075c9c22
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2020-11-18 19:05:11 +0300

    fixed script so it can take kw args (added mock test)

--- homework3/tasks_hw3/task1.py
+++ homework3/tasks_hw3/task1.py
@@ -14,20 +14,19 @@ def cache(times):
     cache_container = {}
 
     def wrapper(func):
-        def inner_f(*args):
-            immut_args = str(args).__hash__()
+        def inner_f(*args, **kwargs):
+            immut_args = (args, str(kwargs)).__hash__()
             if immut_args in cache_container and cache_container[immut_args][1] > 0:
                 output = cache_container[immut_args][0]
                 cache_container[immut_args][1] -= 1
                 if cache_container[immut_args][1] == 0:
                     del cache_container[immut_args]
-                print("cached result")
+
                 return output
             else:
-                result = func(*args)
+                result = func(*args, **kwargs)
                 cache_container[immut_args] = [result, times]
-            print("new result")
-            print(cache_container)
+
             return result
 
         return inner_f
--- homework3/tests/test_task1.py
+++ homework3/tests/test_task1.py
@@ -1,21 +1,36 @@
+from unittest.mock import MagicMock
+
 from tasks_hw3.task1 import cache
 
 
-def some_func(a, b):
-    return (a ** b) ** 2
+@cache(times=3)
+def some_func(a, b, name=None):
+    return (a ** b) ** 2, name
 
 
 def test_cache():
-    test_func = cache(times=3)(some_func)
-    test1 = test_func(100, 200)
-    test2 = test_func(100, 200)
-    test3 = test_func(100, 200)
-    test4 = test_func(100, 200)
-    test5 = test_func(100, 200)
-    test6 = test_func(100, 200)
+
+    test1 = some_func(100, 200, name="keywordarg")
+    test2 = some_func(100, 200, name="keywordarg")
+    test3 = some_func(100, 200, name="keywordarg")
+    test4 = some_func(100, 200, name="keywordarg")
+    test5 = some_func(100, 200, name="keywordarg")
+    test6 = some_func(100, 200, name="keywordarg")
     assert test1 == test2 == test3 == test4 == test5 == test6
     assert test1 is test2, "first output is not cached"
     assert test2 is test3, "second output is not cached"
     assert test3 is test4, "third output is not cached"
     assert test4 is not test5, "output is cached, but must create new object"
     assert test5 is test6, "first output of new input is not cached"
+
+
+def test_cache_with_mock():
+    mock = MagicMock()
+    func = cache(times=4)(mock)
+    func(100, 200, name="keywordarg")
+    func(100, 200, name="keywordarg")
+    func(100, 200, name="keywordarg")
+    func(100, 200, name="keywordarg")
+    func(100, 200, name="keywordarg")
+    func(100, 200, name="keywordarg")
+    assert len(mock.mock_calls) == 2
