ef14554185e2175b9490f4d3a9426b3f7823277f
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2020-11-30 15:58:33 +0300

    changed @type to isinstance func

--- homework5/tests/test_task1_oop_1.py
+++ homework5/tests/test_task1_oop_1.py
@@ -4,7 +4,7 @@ from tasksk_hw5.task1_oop_1 import Homework, Teacher
 
 
 def test_if_create_homework_meth_returns_Homework_obj():
-    assert type(Teacher("a", "b").create_homework("test", 6)) == Homework
+    assert isinstance(Teacher("a", "b").create_homework("test", 6), Homework)
 
 
 def test_if_isactve_meth_returns_false_if_the_task_is_expired():
