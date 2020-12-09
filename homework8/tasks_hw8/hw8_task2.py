62f3f3be5184467f7275afad134f9919c25a72ea
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2020-12-09 17:49:56 +0300

    still to be tested

--- homework8/tasks_hw8/hw8_task2.py
+++ homework8/tasks_hw8/hw8_task2.py
@@ -1,69 +1,99 @@
 import sqlite3
+from itertools import zip_longest
+from typing import Iterator
 
 
 class TableData:
-    def __init__(self, database_name=None, table_name=None):
+    """
+    Wrapper class for database table, that when initialized with database name and table
+    acts as collection object (implements Collection protocol)
+    """
+
+    class TableDataDecorators:
+        @staticmethod
+        def db_conn(func):
+            def wrapper(*args, **kwargs):
+                conn = sqlite3.connect(args[0].database_name)
+                cur = conn.cursor()
+                result = func(*args, **kwargs, bd_connect=cur)
+                conn.commit()
+                conn.close()
+                return result
+
+            return wrapper
+
+    @staticmethod
+    def check_connection(bd_connection):
+        if not bd_connection:
+            raise ConnectionError("No database connection")
+
+    def __init__(self, database_name, table_name):
         self.database_name = database_name
         self.table_name = table_name
-        self.iter_limiter = 0
-
-    def _conn_deco(self):
-        def wrapper(f):
-            conn = sqlite3.connect(self.database_name)
-            cur = conn.cursor()
-            result = f(cur)
-            conn.close()
-            return result
-
-        return wrapper
 
     def __repr__(self):
         return f"{self.__class__.__name__}(database_name={self.database_name}, table_name={self.table_name})"
 
-    @_conn_deco
-    def __len__(self, *args):
-        print(args)
+    @TableDataDecorators.db_conn
+    def __len__(self, bd_connect: sqlite3.Cursor = None) -> int:
+        self.check_connection(bd_connect)
         premade_selection = f"select * from {self.table_name}"
-        return len([_ for _ in cur.execute(premade_selection)])
+        return len([_ for _ in bd_connect.execute(premade_selection)])
 
-    def __getitem__(self, item):
-        conn = sqlite3.connect(self.database_name)
-        cur = conn.cursor()
-        premade_selection = f"select * from {self.table_name} where name=?"
-        return cur.execute(premade_selection, (item,)).fetchall()
+    @TableDataDecorators.db_conn
+    def __getitem__(self, item, bd_connect: sqlite3.Cursor = None):
+        self.check_connection(bd_connect)
+        premade_selection = f"select * from {self.table_name} where name=:name"
+        return bd_connect.execute(premade_selection, (item,)).fetchall()
 
-    def __contains__(self, item):
-        conn = sqlite3.connect(self.database_name)
-        cur = conn.cursor()
-        premade_selection = f"select * from {self.table_name} where name=?"
-        return len(cur.execute(premade_selection, (item,)).fetchall()) > 0
+    @TableDataDecorators.db_conn
+    def __contains__(self, item, bd_connect: sqlite3.Cursor = None):
+        self.check_connection(bd_connect)
+        premade_selection = f"select * from {self.table_name} where name=:name"
+        return len(bd_connect.execute(premade_selection, (item,)).fetchall()) > 0
 
     def __iter__(self):
-        self.limit = 0
+        return TableDataIter(self.database_name, self.table_name)
+
+
+class TableDataIter:
+    def __init__(self, database_name, table_name):
+        self.database_name = database_name
+        self.table_name = table_name
+        self.iter_limiter = 0
+
+    def __iter__(self):
         return self
 
-    def __next__(self):
-        conn = sqlite3.connect(self.database_name)
+    @TableData.TableDataDecorators.db_conn
+    def __next__(self, bd_connect: sqlite3.Cursor = None) -> Iterator:
+        iter_collection = []
         premade_selection = f"select * from {self.table_name}"
-        cur = conn.execute(premade_selection)
-        names = [description[0] for description in cur.description]
-        collect = []
-        fetched_inf = cur.fetchall()
-        for i in fetched_inf:
-            collect.append(dict(zip(names, i)))
-        if self.iter_limiter < len(names):
+        select_result = bd_connect.execute(premade_selection)
+        column_names = [description[0] for description in select_result.description]
+        for element in select_result:
+            iter_collection.append(dict(zip_longest(column_names, element)))
+        if self.iter_limiter < len(iter_collection):
             self.iter_limiter += 1
-            return collect[self.iter_limiter - 1]
+            return iter_collection[self.iter_limiter - 1]
         else:
             raise StopIteration
 
 
 if __name__ == "__main__":
-
+    print("*" * 50)
     presidents = TableData(database_name="example.sqlite", table_name="presidents")
-    # print(presidents)
+    print(presidents)
+    books = TableData(database_name="example.sqlite", table_name="books")
+    print(books)
+    print(len(books))
     print(len(presidents))
-    # print(presidents['Yeltsin'])
-    # print('Yeltsin' in presidents)
-    # for president in presidents:
-    #    print('+',president['name'])
+    print(presidents["Yeltsin"])
+    print("Yeltsin" in presidents)
+    print("*" * 10)
+    for book in books:
+        print(book["name"])
+    iter = iter(books)
+    print(next(iter))
+    print(next(iter))
+    print(next(iter))
