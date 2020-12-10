caf138e9bbab9d7a72afc8b855a0342821f301cb
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2020-12-10 16:26:05 +0300

    added a new test (testing if func in not a mock anymore)

--- homework4/tests/test_task2_hw4.py
+++ homework4/tests/test_task2_hw4.py
@@ -8,7 +8,6 @@ def test_connection():
     mock = MagicMock(return_value=".....")
     tasks_hw4.task2_mock.connect_to_web = mock
 
-    assert isinstance(tasks_hw4.task2_mock.connect_to_web, MagicMock)
     assert tasks_hw4.task2_mock.count_dots_on_i("whatever.com") == 5
     assert mock.mock_calls
 
@@ -20,3 +19,9 @@ def test_failed_connection():
     with pytest.raises(ValueError, match=("Unreachable whatever.com")):
         tasks_hw4.task2_mock.count_dots_on_i("whatever.com")
     assert mock.mock_calls
+
+
+def test_coonect_to_web_not_mock(capsys):
+    print(tasks_hw4.task2_mock.connect_to_web("https://www.google.com/")[0:11])
+    captured = capsys.readouterr()
+    assert captured.out == "GoogleПоиск\n"
