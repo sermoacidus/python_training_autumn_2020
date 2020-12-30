9c693e4ac1a3afd8a3b0b2e35ee8611ce621c754
sermoacidus <51800874+sermoacidus@users.noreply.github.com>
2020-12-30 19:39:46 +0300

    add second migration (with content fields)

--- homework12/dj_proj/dj_app/models.py
+++ homework12/dj_proj/dj_app/models.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+
 from django.db import models
+
+b = timedelta(days=1, hours=2, seconds=15)
 
 
 class Person(models.Model):
@@ -15,8 +19,8 @@ class Student(Person):
 
 
 class Homework(models.Model):
-    text = models.TextField().unique
-    deadline = models.DurationField
+    text = models.TextField(blank=True)
+    deadline = models.IntegerField(default=0)
     created = models.DateField(auto_now_add=True)
     created_by = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
@@ -24,5 +28,5 @@ class Homework(models.Model):
 class HomeworkResult(models.Model):
     given_task = models.ForeignKey(Homework, on_delete=models.CASCADE)
     author = models.ForeignKey(Student, on_delete=models.CASCADE)
-    solution = models.TextField
+    solution = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
--- homework12/dj_proj/scripts/adding_some_info_to_models.py
+++ homework12/dj_proj/scripts/adding_some_info_to_models.py
@@ -13,7 +13,9 @@ def run():
     good_student = Student(first_name="Lev", last_name="Sokolov")
     good_student.save()
 
-    # oop_hw = Homework(text="Learn OOP", deadline=datetime.timedelta(days=1), created_by=oop_teacher)
-    # oop_hw.save()
-    # result_1 = HomeworkResult(given_task=oop_hw, author=lazy_student, solution='I have done this hw ')
-    # result_1.save()
+    oop_hw = Homework(text="Learn OOP", deadline=1, created_by=oop_teacher)
+    oop_hw.save()
+    result_1 = HomeworkResult(
+        given_task=oop_hw, author=lazy_student, solution="I have done this hw "
+    )
+    result_1.save()
